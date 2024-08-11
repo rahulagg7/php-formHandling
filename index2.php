a858f235e392c59ed1ee5767a5446a97969fceec
Rahul Aggarwal <rahulagg217@gmail.com>
2024-08-11 13:19:52 +0530

    php code

---
+++ index2.php
@@ -0,0 +1,23 @@
+<?php
+ echo "my name is rahul";
+ // this is a comment
+ /* this
+    is
+    a 
+    multiline
+    comment
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
